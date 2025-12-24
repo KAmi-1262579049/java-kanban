c03aee9989a50c55ff8674da2448fb8847f9c68f
KAmi-1262579049 <camisha21@yandex.ru>
2025-12-24 20:56:04 +0300

    Delete TaskTracker/src/task directory

--- TaskTracker/src/task/TaskStatus.java
+++
@@ -1,8 +0,0 @@
-package task;
-
-// Перечисление TaskStatus определяет возможные статусы задачи
-public enum TaskStatus {
-    NEW,
-    IN_PROGRESS,
-    DONE
-}
--- TaskTracker/src/task/TaskType.java
+++
@@ -1,8 +0,0 @@
-package task;
-
-// Перечисление TaskType определяет типы задач в системе
-public enum TaskType {
-    TASK,
-    EPIC,
-    SUBTASK
-}
