d2ea4b7f095c232214dc3148c878fc329f5b0500
KAmi-1262579049 <camisha21@yandex.ru>
2026-01-09 16:44:59 +0300

    Update Epic.java

--- src/main/java/task/Epic.java
+++ src/main/java/task/Epic.java
@@ -1,45 +1,42 @@
 package task;
 
-import enums.TaskStatus;
 import java.util.ArrayList;
+import java.util.List;
 
-// Класс эпика наследуется от Task
+// Класс Epic наследуется от Task и представляет сложную задачу, состоящую из подзадач
 public class Epic extends Task {
-    private ArrayList<Integer> subtaskIds; // Список id подзадач, которые входят в эпик
+    private List<Integer> subtaskIds;
 
-    // Конструктор для создания нового эпика
+    // Конструктор для создания нового эпика (без id)
     public Epic(String name, String description) {
-        super(name, description, TaskStatus.NEW);
+        super(name, description);
         this.subtaskIds = new ArrayList<>();
+        this.type = TaskType.EPIC;
+        this.status = TaskStatus.NEW;
     }
 
     // Конструктор для создания эпика с заданными параметрами
-    public Epic(Integer id, String name, String description, TaskStatus status) {
-        super(id, name, description, status);
+    public Epic(int id, String name, String description) {
+        super(id, name, description, TaskStatus.NEW);
         this.subtaskIds = new ArrayList<>();
+        this.type = TaskType.EPIC;
     }
 
-    // Конструктор копирования
-    public Epic(Epic epic) {
-        super(epic);
-        this.subtaskIds = new ArrayList<>(epic.subtaskIds);
+    // Геттер для получения списка идентификаторов подзадач
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds);
     }
 
-    // Геттер для списка id подзадач
-    public ArrayList<Integer> getSubtaskIds() {
-        return subtaskIds;
-    }
-
-    // Метод для добавления id подзадачи в эпик
-    public void addSubtaskId(Integer subtaskId) {
+    // Метод для добавления идентификатора подзадачи в эпик
+    public void addSubtaskId(int subtaskId) {
         if (!subtaskIds.contains(subtaskId)) {
             subtaskIds.add(subtaskId);
         }
     }
 
-    // Метод для удаления id подзадачи из эпика
-    public void removeSubtaskId(Integer subtaskId) {
-        subtaskIds.remove(subtaskId);
+    // Метод для удаления идентификатора подзадачи из эпика
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove(Integer.valueOf(subtaskId));
     }
 
     // Метод для очистки списка подзадач
@@ -47,14 +44,15 @@ public class Epic extends Task {
         subtaskIds.clear();
     }
 
-    // Переопределение метода toString
+    // Переопределение метода toString()
     @Override
     public String toString() {
         return "Epic{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                ", status=" + getStatus() +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", type=" + type +
                 ", subtaskIds=" + subtaskIds +
                 '}';
     }
