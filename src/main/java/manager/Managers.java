cac3bc16e09bb4199a5640b035435c62e6c3b389
KAmi-1262579049 <camisha21@yandex.ru>
2026-01-09 16:42:49 +0300

    Update Managers.java

--- src/main/java/manager/Managers.java
+++ src/main/java/manager/Managers.java
@@ -2,6 +2,11 @@ package manager;
 
 // Класс для создания и управления менеджерами задач
 public class Managers {
+
+    // Приватный конструктор класса
+    private Managers() {
+    }
+
     // Метод для получения реализации менеджера задач по умолчанию
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
